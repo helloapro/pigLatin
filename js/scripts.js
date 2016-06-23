1f7a5292cdb1cbab066ce0f9145d93d4bd81cda3
April Peng and Eric Krause <krause197@gmail.com>
2016-06-22 20:15:27 -0700

    add second spec to put first letter if consonant at end with ay. SUCCESS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,27 @@
-$(function() {
+var pigLatin = function(word) {
+  var v = word.charAt(0);
+  if (v === "a" || v === "e" || v === "i" || v === "o" || v === "u") {
+    var vowelFirst = word.concat("ay");
+    alert(vowelFirst);
+  } else {
+    var leaveFirst = word.substring(word.length - (word.length - 1));
+    var altWord = leaveFirst + word.charAt(0);
+    var consonantFirst = altWord.concat("ay")
+    alert(consonantFirst);
+    //var letters = word.split("");
+    //alert(letters);
+    //alert("what?!");
+  };
+};
 
+$(function() {
   $("form#userInput").submit(function(event) {
     event.preventDefault();
 
     var word = $("input#word").val();
-    var letters = word.split("");
-    alert(letters);
 
+    var result = pigLatin(word);
+    alert(result);
 
-    var vowelFirst = word.concat("ay");
-    alert(vowelFirst);
-
-
-
-
-    });
+  });
 });
